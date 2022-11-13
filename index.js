0ecf2ea9927f7b8f611ba5273cd7ca7ee1d58a14
dhanushvarman <dhanushvarmanj@gmail.com>
2022-11-13 14:47:07 +0530

    cors origin change

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const URL = process.env.DB;
 const app = express();
 
 app.use(cors({
-    origin:"https://6370adc952032d141f835998--iridescent-sable-448c22.netlify.app"
+    origin:"https://iridescent-sable-448c22.netlify.app"
 }))
 
 app.use(express.json());
